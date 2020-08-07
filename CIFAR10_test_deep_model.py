27cc0d91320546468788e8c98ee1200577f53332
Yigit Yukselen <67376236+yigityukselen@users.noreply.github.com>
2020-08-07 13:03:05 +0200

    Add files via upload
    The model is deepened.

---
+++ CIFAR10_test_deep_model.py
@@ -0,0 +1,180 @@
+from __future__ import print_function
+import torch
+import torchvision
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import time
+
+class VoronoiCell():
+	def __init__(self, cell_id):
+		self.id = cell_id
+		# Linear Layers
+		self.Linear_1 = nn.Linear(in_features = 256 * 4 * 4, out_features = 120)
+		self.Linear_2 = nn.Linear(in_features = 120, out_features = 84)
+		self.Linear_3 = nn.Linear(in_features = 84, out_features = 10)
+
+		self.correct_class_numbers = torch.zeros(size = (10, ))
+		self.total_class_numbers = torch.zeros(size = (10, ))
+
+	def forward(self, inp):
+		return self.Linear_3(F.relu(self.Linear_2(F.relu(self.Linear_1(inp)))))	
+
+	def to(self, device):
+		self.Linear_1.to(device)
+		self.Linear_2.to(device)
+		self.Linear_3.to(device)
+
+	def fill_class_numbers(self, prediction_tensor, label_tensor):
+		for idx in range(prediction_tensor.size()[0]):
+			self.total_class_numbers[label_tensor[idx]] += 1
+			if prediction_tensor[idx] == label_tensor[idx]:
+				self.correct_class_numbers[prediction_tensor[idx]] += 1
+
+	def cell_analysis(self, classes):
+		average_accuracy = 0.0
+		print("Head {}:".format(self.id))
+		for idx in range(self.correct_class_numbers.size()[0]):
+			average_accuracy += (100 * self.correct_class_numbers[idx] / self.total_class_numbers[idx]) / self.correct_class_numbers.size()[0]
+			print("\t The classification accuracy on " + classes[idx] + " images is %5.2f %%." %(100 * self.correct_class_numbers[idx] / self.total_class_numbers[idx]))
+		print("\t The average accuracy is: %5.2f %%." % (average_accuracy))
+
+		self.correct_class_numbers = torch.zeros(size = (10, ))
+		self.total_class_numbers = torch.zeros(size = (10, ))
+
+class Net(nn.Module):
+	def __init__(self, num_of_cells):
+		super(Net, self).__init__()
+
+		# Convolutional Layers
+		self.Conv2d_1 = nn.Conv2d(in_channels = 3, out_channels = 6, kernel_size = (3, 3), padding=1, padding_mode='zeros')
+		self.Conv2d_2 = nn.Conv2d(in_channels = 6, out_channels = 16, kernel_size = (3, 3), padding=1, padding_mode='zeros')
+
+		self.Conv2d_3 = nn.Conv2d(in_channels = 16, out_channels = 32, kernel_size = (3, 3), padding=1, padding_mode='zeros')
+		self.Conv2d_4 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = (3, 3), padding=1, padding_mode='zeros')
+
+		self.Conv2d_5 = nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = (3, 3), padding=1, padding_mode='zeros')
+		self.Conv2d_6 = nn.Conv2d(in_channels = 128, out_channels = 256, kernel_size = (3, 3), padding=1, padding_mode='zeros')
+
+		# Linear Layers
+		self.Linear_1 = nn.Linear(in_features = 256 * 4 * 4, out_features = 120)
+		self.Linear_2 = nn.Linear(in_features = 120, out_features = 84)
+		self.Linear_3 = nn.Linear(in_features = 84, out_features = 10)
+
+		# Pooling Layer
+		self.MaxPool2d = nn.MaxPool2d(kernel_size = (2, 2))
+
+		# Voronoi Cells
+		self.number_of_Voronoi_Cells = num_of_cells
+		self.VoronoiCells = []
+		for cell_id in range(self.number_of_Voronoi_Cells):
+			self.VoronoiCells.append(VoronoiCell(cell_id))
+
+	def forward(self, inp):
+		# inp = self.MaxPool2d(F.relu(self.Conv2d_1(inp)))
+		# inp = self.MaxPool2d(F.relu(self.Conv2d_2(inp)))
+		inp = F.relu(self.Conv2d_2(F.relu(self.Conv2d_1(inp))))
+		inp = self.MaxPool2d(inp)
+		inp = F.relu(self.Conv2d_4(F.relu(self.Conv2d_3(inp))))
+		inp = self.MaxPool2d(inp)
+		inp = F.relu(self.Conv2d_6(F.relu(self.Conv2d_5(inp))))
+		inp = self.MaxPool2d(inp)
+		inp = inp.view(-1, 512 * 5 * 5)
+		inp = F.relu(self.Linear_2(F.relu(self.Linear_1(inp))))
+		return self.Linear_3(inp)
+
+	def forward_multiple_hypotheses(self, inp):
+		# inp = self.MaxPool2d(F.relu(self.Conv2d_1(inp)))
+		# inp = self.MaxPool2d(F.relu(self.Conv2d_2(inp)))
+		inp = F.relu(self.Conv2d_2(F.relu(self.Conv2d_1(inp))))
+		inp = self.MaxPool2d(inp)
+		inp = F.relu(self.Conv2d_4(F.relu(self.Conv2d_3(inp))))
+		inp = self.MaxPool2d(inp)
+		inp = F.relu(self.Conv2d_6(F.relu(self.Conv2d_5(inp))))
+		inp = self.MaxPool2d(inp)
+		inp = inp.view(-1, 256 * 4 * 4)
+		predictions = []
+		for i in range(self.number_of_Voronoi_Cells):
+			predictions.append(self.VoronoiCells[i].forward(inp))
+		predictions = torch.stack(tensors=(tuple(predictions)), dim=0)
+		return predictions
+
+	def compute_head_losses(self, hypotheses, labels, criterion):
+		loss_values = []
+		for idx in range(hypotheses.size()[0]):
+			loss = criterion(hypotheses[idx], labels)
+			loss_values.append(loss)
+		loss_values = torch.stack(tensors=(tuple(loss_values)), dim=0)
+		return loss_values
+
+	def compute_meta_losses(self, hypotheses, labels, criterion, min_indices, epsilon, device):
+		for head_idx in range(self.number_of_Voronoi_Cells):
+			delta_values = torch.ones(size=(hypotheses.size()[1], ), device=device)
+			for point_idx in range(hypotheses.size()[1]):
+				if min_indices[point_idx] == head_idx:
+					delta_values[point_idx] *= 1 - epsilon
+				else:
+					delta_values[point_idx] *= epsilon / (self.number_of_Voronoi_Cells - 1)
+
+			loss = criterion(hypotheses[head_idx], labels)
+			loss = torch.mul(loss, delta_values).sum()
+			loss.to(device)
+			loss.backward(retain_graph=True)
+
+
+
+def main():
+	transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
+
+	train_set = torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform, download=True)
+	train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=4, shuffle=True)
+
+	test_set = torchvision.datasets.CIFAR10(root='./data', train=False, transform=transform, download=True)
+	test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=4, shuffle=False)
+
+	classes = ('airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+	net = Net(num_of_cells = 10)
+
+	device = torch.device("cuda")
+	net.to(device)
+	for cell in net.VoronoiCells:
+		cell.to(device)
+	# criterion = nn.CrossEntropyLoss()
+	criterion = nn.CrossEntropyLoss(reduction='none')
+	optimizer = optim.SGD(params=net.parameters(), lr=0.001, momentum=0.9)
+
+	# Training the Network
+	for epoch in range(1, 9):
+		start_time = time.time()
+		for iteration, data in enumerate(train_loader):
+			inputs, labels = data[0].to(device), data[1].to(device)
+			optimizer.zero_grad()
+			hypotheses = net.forward_multiple_hypotheses(inputs) # size = (num_of_heads, batch_size, num_of_classes)
+			hypotheses.to(device)
+			head_losses = net.compute_head_losses(hypotheses, labels, criterion)
+			min_values, min_indices = torch.min(input=head_losses, dim=0)
+			epsilon = 0.2
+			net.compute_meta_losses(hypotheses, labels, criterion, min_indices, epsilon, device)
+			optimizer.step()
+		print("--- %s seconds ---" % (time.time() - start_time))
+		print("Epoch % has finished." % (epoch))
+	print("Finished Training")
+
+	# Testing the Network
+	with torch.no_grad():
+		for data in test_loader:
+			inputs, labels = data[0].to(device), data[1].to(device)
+			hypotheses = net.forward_multiple_hypotheses(inputs)
+			for idx in range(hypotheses.size()[0]):
+				outputs = hypotheses[idx]
+				_, predictions = torch.max(outputs.data,  1)
+				net.VoronoiCells[idx].fill_class_numbers(predictions, labels)
+
+		for idx in range(hypotheses.size()[0]):
+			net.VoronoiCells[idx].cell_analysis(classes)
+	print("Finished Testing")
+
+if __name__ == "__main__":
+	main()
